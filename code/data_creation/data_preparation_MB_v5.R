466fa6893c0f0bd958baeb5720021b5ed318a18f
deskreject <staub_alex@yahoo.com>
2023-09-14 19:29:46 +0200

    created random sample of songs I want to get song characteristics on

--- code/data_creation/data_preparation_MB_v5.R
+++ code/data_creation/data_preparation_MB_v5.R
@@ -29,7 +29,7 @@ indicator_isrc <- sapply(df_musicbrainz_v5$ISRC, function(x) is.null(x) || lengt
 df_musicbrainz_v5_w_isrc <- df_musicbrainz_v5[!indicator_isrc,]
 
 #filter dataframe down to elements that don't include an ISRC
-df_musicbrainz_v5_no_isrc <- df_musicbrainnz_v5[indicator_isrc,]
+df_musicbrainz_v5_no_isrc <- df_musicbrainz_v5[indicator_isrc,]
 
 #investigate the number of MBIDs with ISRCs
 
@@ -56,3 +56,30 @@ table_genres <- table(sapply(df_musicbrianz_v5$genre, function(x) if (is.null(x)
 
 # generate a table of frequencies for number of genres for songs with ISRCs
 table_genres_w_isrcs <- table(sapply(df_musicbrainz_v5_w_isrc$genre, function(x) if (is.null(x)) 0 else length(x)))
+
+####------------------------------ Getting sample of the data with ISRCs ------------------------------####
+
+#draw the random sample
+n <- nrow(df_musicbrainz_v5_w_isrc) # Number of rows in the dataframe
+sample_size <- 1000 # The number of rows you want to sample
+random_rows <- sample(1:n, sample_size, replace = FALSE) # Random row numbers
+random_sample_w_iscrcs_mb_v5 <- df_musicbrainz_v5_w_isrc[random_rows, ] # Subset the dataframe
+
+#unpack the list of ISRCs
+random_sample_w_iscrcs_mb_v5_export <- random_sample_w_iscrcs_mb_v5 %>%
+  unnest(ISRC) %>%
+  dplyr::select(Artist,
+                artist,
+                artist_credit_phrase,
+                Artist_no_featuring,
+                `song title`,
+                `artist mbid`,
+                ISRC,
+                release,
+                recording_id,
+                country,
+                date)
+
+#save the random sample
+write.csv(random_sample_w_iscrcs_mb_v5_export, here::here("data", "interim_data", "random_sample_w_iscrcs_mb_v5_export.csv"))
+
