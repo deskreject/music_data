b15302c16b6eeb58b4b4b3205afc043b2af8ba0d
deskreject <staub_alex@yahoo.com>
2025-09-30 12:38:43 +0200

    "commit in order to be able to install packages"

---
+++ code/exploratory/Spotify_responses/25_09_30_chart_data_spotify_exploration.R
@@ -0,0 +1,16 @@
+#.............................
+# Author: Alexander Staub
+# Description: examining the spotify responses to 2nd spotify scrape (more flexible)
+# Date created: 30-09-2025
+# dependencies:
+## 
+#.............................
+
+
+# loading packages
+library(readr)
+library(here)
+
+# load in the datasets
+
+first_sp_fetch <- read.csv(//bigdata.wu.ac.at/delpero/Data_alexander/data/raw_data/Spotify/1980_2000_songs_artists/final_charted_from_spotify_1980_2000.csv)
